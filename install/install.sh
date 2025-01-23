89a7e180cb9e07b532f747885711a3c44155e9ff
Chandler Thompson <3262190+chandlertee@users.noreplyrgithub.com>
2025-01-23 13:33:54 -0600

    Add install script to install packages and symlink dotfiles

---
+++ install/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install Homebrew and packages
+if ! brew --version &>/dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+brew bundle --file ~/.dotfiles/install/Brewfile --verbose
+
+# Symlink configuration files using Stow
+if ! command -v stow &>/dev/null; then
+    brew install stow
+fi
+stow .
